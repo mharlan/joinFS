7674cb6598ef0342dbb7d523cdbaa7ae23a676ef
mharlan <mharlan@gwmail.gwu.edu>
2010-12-04 02:30:53 -0800

    uuid generator started

---
+++ src/include/jfs_uuid.h
@@ -0,0 +1,9 @@
+#ifndef JOINFS_JFS_UUID_H
+#define JOINFS_JFS_UUID_H
+
+/*
+ * Generates a new unique joinFS id.
+ */
+int jfs_new_uuid();
+
+#endif
---
+++ src/jfs_uuid.c
@@ -0,0 +1,12 @@
+#include "jfs_uuid.h"
+
+#include <uuid/uuid.h>
+
+/*
+ * Generates a new unique joinFS id.
+ */
+int 
+jfs_new_uuid()
+{
+  
+}
