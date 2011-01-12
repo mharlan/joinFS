de7728a2a1f09e5de6931ad8df4bddd6831c9c05
mharlan <mharlan@gwmail.gwu.edu>
2011-01-12 14:51:47 -0800

    forgot file

---
+++ src/include/jfs_db_ops.h
@@ -0,0 +1,38 @@
+/********************************************************************
+ * Copyright 2010, 2011 Matthew Harlan <mharlan@gwmail.gwu.edu>
+ *
+ * This file is part of joinFS.
+ *	 
+ * JoinFS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JoinFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with joinFS.  If not, see <http://www.gnu.org/licenses/>.
+ ********************************************************************/
+#ifndef JOINFS_JFS_DB_OPS_H
+#define JOINFS_JFS_DB_OPS_H
+
+/*
+ * Enumerator used to determine what kind
+ * of database operation is being executed.
+ */
+enum jfs_db_ops {
+  jfs_write_op,
+  jfs_file_cache_op,
+  jfs_key_op,
+  jfs_attr_op,
+  jfs_listattr_op,
+  jfs_dynamic_file_op,
+  jfs_folder_cache_op,
+  jfs_dynamic_folder_op,
+  jfs_search_op
+};
+
+#endif
