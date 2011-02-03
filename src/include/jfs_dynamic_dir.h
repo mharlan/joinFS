ebd81c7aa3e70ef936e4daac784e49266d1fa5fe
mharlan <mharlan@gwmail.gwu.edu>
2011-02-03 09:49:39 -0800

    forgot a file

---
+++ src/include/jfs_dynamic_dir.h
@@ -0,0 +1,15 @@
+#ifndef JOINFS_JFS_DYNAMIC_DIR_H
+#define JOINFS_JFS_DYNAMIC_DIR_H
+
+#define JFS_DIR_XATTR_TRUE   "Y"
+#define JFS_DIR_XATTR_FALSE  "N"
+
+#define JFS_DIR_IS_DYNAMIC   "_jfs_dir_is_dynamic"
+#define JFS_DIR_HAS_SUBQUERY "_jfs_dir_has_subquery"
+#define JFS_DIR_IS_SUBQUERY  "_jfs_dir_is_subquery"
+#define JFS_DIR_PATH_ITEMS   "_jfs_dir_path_items"
+#define JFS_DIR_SUB_INODE    "_jfs_dir_sub_inode"
+#define JFS_DIR_SUB_KEY      "_jfs_dir_sub_key"
+#define JFS_DIR_QUERY        "_jfs_dir_query"
+
+#endif
